8f8705d504539fb79c700fd88ee933c8df414c50
MarkLinAtTelmar <cmark@telmar.com>
2024-02-22 17:22:55 +0800

    Create log_utils.py

---
+++ log_utils.py
@@ -0,0 +1,11 @@
+import logging
+
+class EngineLog():
+    def __init__(self, name) -> None:
+        self.__logger = None
+        self.__logger_name = name
+
+    def get_logger(self):
+        if self.__logger is None:
+            self.__logger = logging.getLogger(self.__logger_name)
+        return self.__logger
