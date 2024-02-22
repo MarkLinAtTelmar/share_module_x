6348f1fcb1d330fe392c45073a4e4988af369ed3
MarkLinAtTelmar <cmark@telmar.com>
2024-02-22 17:32:25 +0800

    Update log_utils.py

--- log_utils.py
+++ log_utils.py
@@ -9,3 +9,6 @@ class EngineLog():
         if self.__logger is None:
             self.__logger = logging.getLogger(self.__logger_name)
         return self.__logger
+
+    def print_on_console(self, message):
+        print(message)
