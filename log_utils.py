a78e96b411447dfd6f98dc96e4c51946b17ec57f
MarkLinAtTelmar <cmark@telmar.com>
2024-02-22 17:31:14 +0800

    Add test lambda

--- log_utils.py
+++ log_utils.py
@@ -2,10 +2,13 @@ import logging
 
 class EngineLog():
     def __init__(self, name) -> None:
-        self.__logger = None
+        self.__logger:logging.Logger = None
         self.__logger_name = name
 
-    def get_logger(self):
+    def get_logger(self) -> logging.Logger:
         if self.__logger is None:
             self.__logger = logging.getLogger(self.__logger_name)
         return self.__logger
+
+    def print_debug_log(self, message):
+        self.get_logger().debug(message)
